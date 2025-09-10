3613ae48204288f87627731d0705599c0862f06a
tuanldas <tuanldas@gmail.com>
2025-09-10 14:04:23 +0700

    .

--- app/(protected)/components/demo1/light-sidebar/demo1-light-sidebar-page.tsx
+++ app/(protected)/components/demo1/light-sidebar/demo1-light-sidebar-page.tsx
@@ -1,93 +1,91 @@
-import { Fragment, useState } from 'react';
+import {Fragment, useState} from 'react';
+import {addDays, format} from 'date-fns';
+import {CalendarDays} from 'lucide-react';
+import {DateRange} from 'react-day-picker';
+import {Button} from '@/components/ui/button';
+import {Calendar} from '@/components/ui/calendar';
+import {Popover, PopoverContent, PopoverTrigger,} from '@/components/ui/popover';
+import {Container} from '@/components/common/container';
+import {Demo1LightSidebarContent} from './';
 import {
-  Toolbar,
-  ToolbarActions,
-  ToolbarHeading,
-} from '@/layouts/demo1/components/toolbar';
-import { addDays, format } from 'date-fns';
-import { CalendarDays } from 'lucide-react';
-import { DateRange } from 'react-day-picker';
-import { Button } from '@/components/ui/button';
-import { Calendar } from '@/components/ui/calendar';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
-import { Container } from '@/components/common/container';
-import { Demo1LightSidebarContent } from './';
+    Toolbar,
+    ToolbarActions,
+    ToolbarDescription,
+    ToolbarHeading,
+    ToolbarPageTitle
+} from '@/app/components/layouts/layout-11/components/toolbar';
 
 export function Demo1LightSidebarPage() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [date, setDate] = useState<DateRange | undefined>({
-    from: new Date(2025, 0, 20),
-    to: addDays(new Date(2025, 0, 20), 20),
-  });
-  const [tempDateRange, setTempDateRange] = useState<DateRange | undefined>(
-    date,
-  );
+    const [isOpen, setIsOpen] = useState(false);
+    const [date, setDate] = useState<DateRange | undefined>({
+        from: new Date(2025, 0, 20),
+        to: addDays(new Date(2025, 0, 20), 20),
+    });
+    const [tempDateRange, setTempDateRange] = useState<DateRange | undefined>(
+        date,
+    );
 
-  const handleDateRangeApply = () => {
-    setDate(tempDateRange); // Save the temporary date range to the main state
-    setIsOpen(false); // Close the popover
-  };
+    const handleDateRangeApply = () => {
+        setDate(tempDateRange); // Save the temporary date range to the main state
+        setIsOpen(false); // Close the popover
+    };
 
-  const handleDateRangeReset = () => {
-    setTempDateRange(undefined); // Reset the temporary date range
-  };
+    const handleDateRangeReset = () => {
+        setTempDateRange(undefined); // Reset the temporary date range
+    };
 
-  const defaultStartDate = new Date(); // Default start date fallback
+    const defaultStartDate = new Date(); // Default start date fallback
 
-  return (
-    <Fragment>
-      <Container>
-        <Toolbar>
-          <ToolbarHeading
-            title="Dashboard"
-            description="Central Hub for Personal Customization"
-          />
-          <ToolbarActions>
-            <Popover open={isOpen} onOpenChange={setIsOpen}>
-              <PopoverTrigger asChild>
-                <Button id="date" variant="outline">
-                  <CalendarDays size={16} className="me-0.5" />
-                  {date?.from ? (
-                    date.to ? (
-                      <>
-                        {format(date.from, 'LLL dd, y')} -{' '}
-                        {format(date.to, 'LLL dd, y')}
-                      </>
-                    ) : (
-                      format(date.from, 'LLL dd, y')
-                    )
-                  ) : (
-                    <span>Pick a date range</span>
-                  )}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="end">
-                <Calendar
-                  initialFocus
-                  mode="range"
-                  defaultMonth={tempDateRange?.from || defaultStartDate}
-                  selected={tempDateRange}
-                  onSelect={setTempDateRange}
-                  numberOfMonths={2}
-                />
-                <div className="flex items-center justify-end gap-1.5 border-t border-border p-3">
-                  <Button variant="outline" onClick={handleDateRangeReset}>
-                    Reset
-                  </Button>
-                  <Button onClick={handleDateRangeApply}>Apply</Button>
-                </div>
-              </PopoverContent>
-            </Popover>
-          </ToolbarActions>
-        </Toolbar>
-      </Container>
-      <Container>
-        <Demo1LightSidebarContent />
-      </Container>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Container>
+                <Toolbar>
+                    <ToolbarHeading>
+                        <ToolbarPageTitle>Dashboard</ToolbarPageTitle>
+                        <ToolbarDescription>Central Hub for Personal Customization</ToolbarDescription>
+                    </ToolbarHeading>
+                    <ToolbarActions>
+                        <Popover open={isOpen} onOpenChange={setIsOpen}>
+                            <PopoverTrigger asChild>
+                                <Button id="date" variant="outline">
+                                    <CalendarDays size={16} className="me-0.5"/>
+                                    {date?.from ? (
+                                        date.to ? (
+                                            <>
+                                                {format(date.from, 'LLL dd, y')} -{' '}
+                                                {format(date.to, 'LLL dd, y')}
+                                            </>
+                                        ) : (
+                                            format(date.from, 'LLL dd, y')
+                                        )
+                                    ) : (
+                                        <span>Pick a date range</span>
+                                    )}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-auto p-0" align="end">
+                                <Calendar
+                                    initialFocus
+                                    mode="range"
+                                    defaultMonth={tempDateRange?.from || defaultStartDate}
+                                    selected={tempDateRange}
+                                    onSelect={setTempDateRange}
+                                    numberOfMonths={2}
+                                />
+                                <div className="flex items-center justify-end gap-1.5 border-t border-border p-3">
+                                    <Button variant="outline" onClick={handleDateRangeReset}>
+                                        Reset
+                                    </Button>
+                                    <Button onClick={handleDateRangeApply}>Apply</Button>
+                                </div>
+                            </PopoverContent>
+                        </Popover>
+                    </ToolbarActions>
+                </Toolbar>
+            </Container>
+            <Container>
+                <Demo1LightSidebarContent/>
+            </Container>
+        </Fragment>
+    );
 }
