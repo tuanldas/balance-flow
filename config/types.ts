7066f01f53dc75c7477ae5261715ce31250ec0b6
tuanldas <tuanldas@gmail.com>
2025-09-11 11:16:34 +0700

    refactor(settings): tối giản Settings và APP_SETTINGS chỉ còn layout-11

--- config/types.ts
+++ config/types.ts
@@ -21,26 +21,4 @@ export type MenuConfig = MenuItem[];
 export interface Settings {
     container: 'fixed' | 'fluid';
     layout: string;
-    layouts: {
-        demo1: {
-            sidebarCollapse: boolean;
-            sidebarTheme: 'light' | 'dark';
-        };
-        demo2: {
-            headerSticky: boolean;
-            headerStickyOffset: number;
-        };
-        demo5: {
-            headerSticky: boolean;
-            headerStickyOffset: number;
-        };
-        demo7: {
-            headerSticky: boolean;
-            headerStickyOffset: number;
-        };
-        demo9: {
-            headerSticky: boolean;
-            headerStickyOffset: number;
-        };
-    };
 }
