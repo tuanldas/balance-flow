9b6bca23feb66e90ae51281a3c2bcbd9c413ee4e
tuanldas <tuanldas@gmail.com>
2025-09-11 11:18:36 +0700

    chore(i18n): giữ lại tiếng Anh, xoá messages ngôn ngữ khác

--- i18n/config.ts
+++ i18n/config.ts
@@ -15,32 +15,4 @@ export const I18N_LANGUAGES: Language[] = [
         direction: 'ltr',
         flag: '/media/flags/united-states.svg',
     },
-    {
-        code: 'ar',
-        name: 'Arabic',
-        shortName: 'AR',
-        direction: 'rtl',
-        flag: '/media/flags/saudi-arabia.svg',
-    },
-    {
-        code: 'es',
-        name: 'Spanish',
-        shortName: 'ES',
-        direction: 'ltr',
-        flag: '/media/flags/spain.svg',
-    },
-    {
-        code: 'de',
-        name: 'German',
-        shortName: 'DE',
-        direction: 'ltr',
-        flag: '/media/flags/germany.svg',
-    },
-    {
-        code: 'ch',
-        name: 'Chinese',
-        shortName: 'CH',
-        direction: 'ltr',
-        flag: '/media/flags/china.svg',
-    },
 ];
