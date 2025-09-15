10682a2dc49825d71780685538382d007ee7e75e
tuanldas <tuanldas@gmail.com>
2025-09-15 15:16:24 +0700

    feat(wallets): mở dropdown trên hover (control open state)

--- app/components/partials/dropdown-menu/dropdown-menu-wallets.tsx
+++ app/components/partials/dropdown-menu/dropdown-menu-wallets.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 
 export function WalletsDropdownMenu({ trigger }: { trigger: ReactNode }) {
+    const [open, setOpen] = useState(false);
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
-            <DropdownMenuContent className="w-[175px]" side="bottom" align="end" />
-        </DropdownMenu>
+        <div onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} className="inline-flex">
+            <DropdownMenu open={open} onOpenChange={setOpen}>
+                <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+                <DropdownMenuContent className="w-[175px]" side="bottom" align="end" />
+            </DropdownMenu>
+        </div>
     );
 }
 
